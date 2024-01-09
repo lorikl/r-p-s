fb6ff0a7ef9b8b93cf48a40414d6e49eb6f1f957
Lorik Latifi <Lorikkatifi@gmail.com>
2024-01-08 22:44:48 -0500

    Let User Choose from HTML

--- script.js
+++ script.js
@@ -1,30 +1,62 @@
+let rock = document.querySelector('.rock')
+let paper = document.querySelector('.paper')
+let scissors = document.querySelector('.scissors')
+let text = document.querySelector('.text')
+let playerScore = document.querySelector('#playerScore')
+let cpuScore = document.querySelector('#cpuScore')
+let playerWins = 0;
+let computerWins = 0;
+
 function getComputerChoice () {
     let arr = ["ROCK", "PAPER", "SCISSORS"]
     return arr[Math.floor(Math.random() * 3)]
 }
-const playerChoice = prompt('Pick a Choice').toUpperCase()
+
 const cpuChoice = getComputerChoice()
 
 function playRound(playerChoice, getComputerChoice) {
     if (playerChoice === getComputerChoice) {
-      return 'tie'
+      text.innerHTML = playerChoice+" and "+getComputerChoice+ ' equals a TIE'
     }
     if (
       (playerChoice === 'ROCK' && getComputerChoice === 'SCISSORS') ||
       (playerChoice === 'SCISSORS' && getComputerChoice === 'PAPER') ||
       (playerChoice === 'PAPER' && getComputerChoice === 'ROCK')
     ) {
-      return 'player WINS'
+        playerWins++
+        playerScore.innerHTML = playerWins
+      text.innerHTML = playerChoice+' beats '+getComputerChoice+ '\n player WINS'
     }
     if (
       (getComputerChoice === 'ROCK' && playerChoice === 'SCISSORS') ||
       (getComputerChoice === 'SCISSORS' && playerChoice === 'PAPER') ||
       (getComputerChoice === 'PAPER' && playerChoice === 'ROCK')
     ) {
-      return 'computer WINS'
+        computerWins ++
+        cpuScore.innerHTML = computerWins
+      text.innerHTML = playerChoice+' losses to '+getComputerChoice+ '\n computer WINS'
     }
-  }
+}
+rock.addEventListener('click', () => handleClick('ROCK'))
+paper.addEventListener('click', () => handleClick('PAPER'))
+scissors.addEventListener('click', () => handleClick('SCISSORS'))
 
-console.log(cpuChoice)
-console.log(playerChoice)
-console.log(playRound(playerChoice, cpuChoice))
+function handleClick(playerChoice){
+    const cpuChoice = getComputerChoice()
+    playRound(playerChoice, cpuChoice)
+}
+
+text.innerHTML = handleClick()
+
+// function endGame() {
+//     if (playerScore == 5){
+//         text.innerHTML = "YOU WIN THE GAME!"
+//         playerScore = 0
+//         cpuScore = 0
+//     } else if (cpuScore == 5){
+//         text.innerHTML = "YOU LOSE THE GAME, TRY AGAIN"
+//         playerScore = 0
+//         cpuScore = 0
+//     }
+// }
+
